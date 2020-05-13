a74f35774072746362c2a8019db6f252d5527b11
GhassenMedyouni <ghassen.medyouni@gmail.com>
2020-05-14 01:41:44 +0200

    add...

--- app/src/main/java/com/example/miniprojet/UsersFragment.java
+++ app/src/main/java/com/example/miniprojet/UsersFragment.java
@@ -17,7 +17,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
