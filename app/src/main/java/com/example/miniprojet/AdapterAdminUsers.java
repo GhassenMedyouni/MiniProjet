a8688e1a29875da652637af5961383373a96894e
GhassenMedyouni <ghassen.medyouni@gmail.com>
2020-05-19 15:21:43 +0200

    add...

--- app/src/main/java/com/example/miniprojet/AdapterAdminUsers.java
+++ app/src/main/java/com/example/miniprojet/AdapterAdminUsers.java
@@ -1,6 +1,7 @@
 package com.example.miniprojet;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,10 +11,20 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -43,7 +54,7 @@ public class AdapterAdminUsers extends  RecyclerView.Adapter<AdapterAdminUsers.M
     public void onBindViewHolder(@NonNull MyHolder holder, int i) {
         //get data
         final String hisUID = userList2.get(i).getUid();
-        String userImage = userList2.get(i).getImage();
+        final String userImage = userList2.get(i).getImage();
         String userName = userList2.get(i).getName();
         final String userEmail = userList2.get(i).getEmail();
 
@@ -80,7 +91,96 @@ public class AdapterAdminUsers extends  RecyclerView.Adapter<AdapterAdminUsers.M
             }
         });
 
+        holder.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final  ProgressDialog pd = new ProgressDialog(context);
+                pd.setMessage("Deleting");
+                Query fquery = FirebaseDatabase.getInstance().getReference("Users").orderByChild("uid").equalTo(hisUID);
+                fquery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds: dataSnapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(context,"Deleted successfully", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
+
+ /**   private void beginDelete(String hisUID, String userImage) {
+        if(userImage.equals("noImage")){
+            deleteWithoutImage(hisUID);
+        }
+        else {
+            deleteWithImage(hisUID, userImage);
+
+        }
+    }
+
+    private void deleteWithoutImage(String hisUID) {
+        final ProgressDialog pd = new ProgressDialog(context);
+        pd.setMessage("Deleting");
+        Query fquery = FirebaseDatabase.getInstance().getReference("Users").orderByChild("uid").equalTo(hisUID);
+        fquery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    ds.getRef().removeValue();
+                }
+                Toast.makeText(context,"Deleted successfully", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+/**
+    private void deleteWithImage(final String hisUID, String userImage) {
+        final ProgressDialog pd = new ProgressDialog(context);
+        pd.setMessage("Deleting");
+
+        StorageReference picRef = FirebaseStorage.getInstance().getReferenceFromUrl(userImage);
+        picRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Query fquery = FirebaseDatabase.getInstance().getReference("Users").orderByChild("uid").equalTo(hisUID);
+                fquery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds: dataSnapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(context,"Deleted successfully", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }  **/
 
     @Override
     public int getItemCount() {
@@ -100,6 +200,7 @@ public class AdapterAdminUsers extends  RecyclerView.Adapter<AdapterAdminUsers.M
            mAvatarIvAd = itemView.findViewById(R.id.avatarIvAd);
            mNameTvAd = itemView.findViewById(R.id.nameTvAd);
            mEmailTvAd = itemView.findViewById(R.id.emailTvAd);
+           deleteBtn = itemView.findViewById(R.id.deleteBtn);
        }
    }
 
